a346a78b29c0da51f1b14ab2a99d4b94bce57c6e
xiongfeng <2318452499@qq.com>
2021-10-13 19:21:17 +0800

    xf

--- App.vue
+++ App.vue
@@ -15,4 +15,5 @@
 <style>
 	/*每个页面公共css */ 
 	@import './common/uni.css';
+	@import './static/css/iconfont.css';
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,40 +1,35 @@
 <template>
 	<view>
-		<view class="uni-margin-wrap">
-			<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
-				<swiper-item>
-					<view class="swiper-item"><image mode="widthFix" src="../../static/images/yuyue.png"></image></view>
-				</swiper-item>
-			</swiper>
+		<view class="notificationBanner">
+			<banner :list="bannerList" :themeColor="themeColor" @toDetailPage="toDetailPage"></banner>
 		</view>
-		
 		<view class="notificationBar">
-			<view class="item">
-				<gbro-marquee broadcastType='text' direction="top" :viewHeight="100"
-				 :broadcastIconIsDisplay="!true" :touchEvent="true" :broadcastData='broadcastData' :broadcastStyle='broadcastStyle'
-				 style="width: 100%">
-				</gbro-marquee>
-			</view>
+			<goUpNotice :list="goUpNoticeList" :themeColor="themeColor" :haveMore="false"></goUpNotice>
 		</view>
-			
-		<view>
+
+		<view class="content">
 			<view style="text-align: center;font-weight: 600;font-size: 40rpx;">今日预约额度仅剩</view>
 			<view style="text-align: center;font-weight: 600;font-size: 40rpx;">145万元</view>
 		</view>
-		
-		
-		
-		
 
-		
-		<button type="primary" @click="toAppointmentPage()" style="width: 120px;background-color: #ED1C24;">开始预约</button>
+
+
+
+
+
+		<button type="primary" @click="toAppointmentPage()"
+			style="width: 120px;background-color: #ED1C24;">开始预约</button>
 	</view>
 </template>
 <script>
-	import gbroMarquee from '@/components/gbro-marquee/marquee.vue'
+	import gbroMarquee from '@/components/gbro-marquee/marquee.vue';
+	import goUpNotice from '@/components/notice-goUp/notice-goUp.vue';
+	import banner from '@/components/ay-banner/banner.vue';
 	export default {
 		components: {
-		    gbroMarquee
+			gbroMarquee,
+			goUpNotice,
+			banner
 		},
 		data() {
 			return {
@@ -42,27 +37,49 @@
 				autoplay: true,
 				interval: 1000,
 				duration: 1000,
-				broadcastData: [111111111,2222222,333333],
-				broadcastStyle: {
-				    speed: 12, //每秒3个字
-				    font_size: "32", //字体大小(rpx)
-					text_color: "#666666", //字体颜色
-				    back_color: "#99999", //背景色
-				
-				}
+				themeColor: '#f2f2f2',
+				goUpNoticeList: [{
+						directOne1: '路遥知马力，日久见人心',
+						directOne2: '《元曲选·争报恩》',
+					}, {
+						directOne1: '天生我材必有用',
+						directOne2: '李白',
+					},
+					{
+						directOne1: '穷则独善其身，达则兼济天下',
+						directOne2: '孟子',
+					}, {
+						directOne1: '业精于勤，荒于嬉；行成于思，毁于随',
+						directOne2: '韩愈',
+					}, {
+						directOne1: '一年之计在于春，一日之计在于晨',
+						directOne2: '萧绎',
+					},
+				],
+				bannerList: [{
+						id: 0,
+						img: '../../static/images/banner.jpg',
+						url: 'www.baidu.com/',
+
+					},
+					{
+						id: 1,
+						img: 'https://cdn.pixabay.com/photo/2016/11/23/17/55/beach-1854072__340.jpg',
+						url: 'www.baidu.com/',
+					},
+					{
+						id: 2,
+						img: 'https://cdn.pixabay.com/photo/2016/11/19/12/25/art-1839006__340.jpg',
+						url: 'www.baidu.com/',
+					},
+				],
 			}
 		},
 		onLoad() {
-		                let that = this;
-		                setTimeout(() => { //模拟接口请求
-		                    that.broadcastData = ['温**                      成功预约 10 万元',
-		                        '李**                 成功预约 10 万元', '年年答复是短发分公司噶阿飞嘎斯在'
-		                    ]
-		                    
-		                }, 1000)
-		            },
+
+		},
 		methods: {
-			toAppointmentPage(){
+			toAppointmentPage() {
 				uni.navigateTo({
 					url: '../feature/appointment'
 				})
@@ -78,12 +95,38 @@
 			},
 			durationChange(e) {
 				this.duration = e.detail.value
-			}
+			},
+			toDetailPage(e) {
+				let list = e.list;
+				let idx = e.curIndex;
+				let list_img = [];
+				list.forEach(item => {
+					list_img.push(item.img)
+				})
+				if (list_img && list_img.length > 0) {
+					uni.previewImage({
+						current: list_img[idx],
+						// 传 Number H5端出现不兼容 
+						urls: list_img,
+						indicator: "number",
+						loop: true,
+					});
+				}
+			},
+
 		}
 	}
 </script>
 
 <style>
+	/* page {
+		background-color: #f2f2f2;
+	} */
+	/* 轮播 */
+	.notificationBanner{
+		padding: 0 30rpx;
+		margin-top: 30rpx;
+	}
 	/* 通知 */
 	.notificationBar {
 		padding: 0 30rpx;
@@ -91,50 +134,7 @@
 		height: 500rpx;
 		color: #e54059;
 	}
-	.swiper_container {
-		height: 360rpx !important;
-		width: 100%;
-		line-height: 50rpx;
-		font-size: 30rpx;
-	}
-	.swiper_item {
-		font-size: 30rpx;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		white-space: nowrap;
-		margin-left: 5rpx;
-	}
-	.uni-margin-wrap {
-		width:690rpx;
-		margin:0 30rpx;
-	}
-	.swiper {
-		height: 300rpx;
-	}
-	.swiper-item {
-		display: block;
-		height: 300rpx;
-		line-height: 300rpx;
-		text-align: center;
-	}
+	
 
-	.swiper-list {
-		margin-top: 40rpx;
-		margin-bottom: 0;
-	}
-
-	.uni-common-mt{
-		margin-top:60rpx;
-		position:relative;
-	}
-
-	.info {
-		position: absolute;
-		right:20rpx;
-	}
-
-    .uni-padding-wrap {
-        width:550rpx;
-        padding:0 100rpx;
-    }
+	
 </style>
