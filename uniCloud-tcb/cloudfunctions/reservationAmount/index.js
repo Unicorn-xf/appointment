ac5a8dff1bc15f4a10f2e1ede322de155a92d035
熊锋 <2318452499@qq.com>
2022-09-27 14:00:20 +0800

    调整

--- uniCloud-tcb/cloudfunctions/reservationAmount/index.js
+++ uniCloud-tcb/cloudfunctions/reservationAmount/index.js
@@ -1,9 +1,25 @@
 'use strict';
 const db = uniCloud.database();
+const tools = require('tools');
 exports.main = async (event, context) => {
 	//event为客户端上传的参数
-	console.log('event : ', event)
-	
-	//返回数据给客户端
-	return event
+	switch (event.method) {
+		case 'getReservationAmount':
+			return getReservationAmount(event.data)
+			break;
+		default:
+			break;
+	}
 };
+
+
+async function getReservationAmount(data) {
+	try {
+		
+		let list = await db.collection('reservationAmount').limit(10000).get();
+		console.log("=====:"+list.data);
+		//return tools.serverSuccess(list.data);
+	} catch (err) {
+		return err.message
+	}
+}
